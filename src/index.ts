a832e4895b23a1abea9f61a240f567cfded8e560
shruthik shetty <shrutikshetty14@gmail.com>
2025-05-16 21:22:50 +0530

    express-typescript-template

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// main starting point
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+// configure .env
+dotenv.config();
+const app = express();
+
+// Middleware for parsing JSON request bodies
+app.use(express.json());
+
+// Example Route
+app.get('/', (req, res: Response) => {
+  res.send('Hello TypeScript with Express! Enjoy');
+});
+export const getExample = (req: Request, res: Response) => {
+  res.json({ message: 'This is a sample endpoint' });
+};
+// Example Route to test a controller
+app.get('/example', getExample);
+
+// Start the server
+export default app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
+});
